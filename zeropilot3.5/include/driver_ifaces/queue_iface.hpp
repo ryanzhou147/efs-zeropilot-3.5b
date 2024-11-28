52d85bee67b09f1607b0488a65d3afd66bb8ebfc
peng-dk <dkpeng@uwaterloo.ca>
2024-11-28 15:13:40 -0500

    created abstract interface for msg queue interface (#10)
    Co-authored-by: Dami Peng <yasai@Damis-MacBook-Air.local>

---
+++ zeropilot3.5/include/driver_ifaces/queue_iface.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+template <typename T>
+class IMessageQueue {
+    protected: 
+        IMessageQueue() = default;
+    public: 
+        virtual ~IMessageQueue() = default;
+        // retrieves the top of the queue
+        virtual T get() = 0;
+        // pushes template message to the back of the queue
+        virtual T push(T message) = 0;
+        // pops the top of the queue
+        virtual T pop() = 0;
+        // counts the # of items in the queue
+        virtual int count() = 0;
+};
