6d38b3f159694e808f7109100c696b2757d00b0b
Raiyan Samin <rrsamin@uwaterloo.ca>
2024-12-10 19:09:47 -0500

    feature/sm/failsafe (#7)
    * Implemented failsafe
    
    * Added logic to go into failsafe
    
    * Added a mutable threshold
    
    * Fixed any conflicts after rebasing
    
    * fixed changes
    
    * changed invalid threshold to uint
    
    * fixed variable names
    
    * format non member variables
    
    ---------
    
    Co-authored-by: Derek Tang <d6tang@uwaterloo.ca>

--- zeropilot3.5/include/system_manager/system_manager.hpp
+++ zeropilot3.5/include/system_manager/system_manager.hpp
@@ -6,17 +6,18 @@
 
 class SystemManager {
     public:
-    SystemManager(SBusIface *rc_driver, QueueIface<RCMotorControlMessage_t> *queue_driver);
-    ~SystemManager();
+        SystemManager() = delete;
+        SystemManager(ISBUSReceiver *rcDriver, IMessageQueue<RCMotorControlMessage_t> *queueDriver, uint32_t invalidThreshold);
 
-    void SMUpdate(); // This function is the main function of SM, it should be called in the main loop of the system.
+        void SMUpdate(); // This function is the main function of SM, it should be called in the main loop of the system.
 
     private:
-        SBusIface *rc_driver_;
-        QueueIface<RCMotorControlMessage_t> *queue_driver_;
+        ISBUSReceiver *rcDriver_;
+        IMessageQueue<RCMotorControlMessage_t> *queueDriver_;
+        uint32_t invalidThreshold_;
 
-        int16_t invalidRCCount_ = 0;
+        uint32_t invalidRCCount_ = 0;
 
-        void sendRCDataToAttitudeManager(const SBusIface::RCData_t &rcData);
+        void sendRCDataToAttitudeManager(const RCControl_t &rcData);
         void sendDisarmedToAttitudeManager();
 };
--- zeropilot3.5/src/system_manager/system_manager.cpp
+++ zeropilot3.5/src/system_manager/system_manager.cpp
@@ -1,18 +1,34 @@
 #include "system_manager.hpp"
 
-SystemManager::SystemManager(SBusIface *rc_driver, QueueIface<RCMotorControlMessage_t> *queue_driver)
-    : rc_driver_(rc_driver), queue_driver_(queue_driver) {}
-
-SystemManager::~SystemManager() {}
+SystemManager::SystemManager(ISBUSReceiver *rcDriver, IMessageQueue<RCMotorControlMessage_t> *queueDriver, uint32_t invalidThreshold)
+    : rcDriver_(rcDriver), queueDriver_(queueDriver), invalidRCCount_(invalidThreshold) {}
 
 void SystemManager::SMUpdate() {
-    // TODO: Implement this function
+    RCControl_t rcData = rcDriver_->getRCData();
+    
+    // If no new data is recieved for some time, go to failsafe
+    if (!rcData.isDataNew) {
+        invalidRCCount_++;
+        if (invalidRCCount_ > invalidRCCount_) {
+            sendDisarmedToAttitudeManager();
+        }
+    } 
+    else {
+        invalidRCCount_ = 0;
+    }
 }
 
-void SystemManager::sendRCDataToAttitudeManager(const SBusIface::RCData_t &rcData) {
+void SystemManager::sendRCDataToAttitudeManager(const RCControl_t &rcData) {
     // TODO: Implement this function
 }
 
 void SystemManager::sendDisarmedToAttitudeManager() {
-    // TODO: Implement this function
+    RCMotorControlMessage_t disarmedMessage;
+
+    disarmedMessage.roll = 0.0f;
+    disarmedMessage.pitch = 0.0f;
+    disarmedMessage.yaw = 0.0f;
+    disarmedMessage.throttle = -1.0f;
+
+    queueDriver_->push(disarmedMessage);
 }
