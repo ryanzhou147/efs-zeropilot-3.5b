bd19936c07e2c0146c9ae4227b0d533d9b7968bd
Rosnaky <ronakjpatelr22@gmail.com>
2025-03-03 17:17:03 -0500

    added dma callback

--- stm32l552xx/boardfiles/override.cpp
+++ stm32l552xx/boardfiles/override.cpp
@@ -6,16 +6,20 @@ extern "C"
 /* overriding _write to redirect puts()/printf() to SWO */
 int _write(int file, char *ptr, int len)
 {
-  if( osMutexAcquire(itmMutex, osWaitForever) == osOK )
-  {
-    for (int DataIdx = 0; DataIdx < len; DataIdx++)
-    {
-      ITM_SendChar(ptr[DataIdx]);
+    if (osMutexAcquire(itmMutex, osWaitForever) == osOK) {
+        for (int DataIdx = 0; DataIdx < len; DataIdx++) {
+            ITM_SendChar(ptr[DataIdx]);
+        }
+        osMutexRelease(itmMutex);
     }
-    osMutexRelease(itmMutex);
-  }
-  return len;
+    return len;
 }
 
 /* interrupt callback functions */
+void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size) {
+    // GPS dma callback
+    if (&huart2 == huart) {
+      gps->processGPSData();
+    }
+}
 }
