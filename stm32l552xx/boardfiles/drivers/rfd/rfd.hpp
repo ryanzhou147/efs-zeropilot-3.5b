0813fdcc1d87eafa61e5d64c047a1f8e0fc9c3f2
Aditya Bang <adityabang15@gmail.com>
2025-03-09 19:08:54 -0400

    rfd basic file structure

---
+++ stm32l552xx/boardfiles/drivers/rfd/rfd.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <stm32l5xx_hal_uart.h>
+#include "rfd_iface.hpp"
+
+#ifdef __cplusplus
+#include <cstdint>
+#endif
+
+
+class RFD : public IRFD {
+
+public:
+
+private:
+
+};
---
+++ zeropilot3.5/include/driver_ifaces/rfd_iface.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifdef __cplusplus
+#include <cstdint>
+#endif
+
+
+class IRFD {
+protected:
+	IRFD() = default;
+
+public:
+	virtual ~IRFD() = default;
+};
