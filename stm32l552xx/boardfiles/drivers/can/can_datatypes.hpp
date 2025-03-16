175a422401cb8961d3a884a402d0de9547e8564e
Rosnaky <ronakjpatelr22@gmail.com>
2025-03-16 15:51:15 -0400

    update node structure

--- stm32l552xx/boardfiles/drivers/can/can_datatypes.hpp
+++ stm32l552xx/boardfiles/drivers/can/can_datatypes.hpp
@@ -2,8 +2,11 @@
 #include <cstdint>
 
 
-// TODO: complete
 typedef struct {
 	uint8_t nodeId;
-	bool active;
+	int health;
+	uint32_t lastHeartbeatTime;
+	uint32_t uptime;
+	uint32_t softwareVersion;
+	uint32_t hardwareVersion;
 } Node_t;
