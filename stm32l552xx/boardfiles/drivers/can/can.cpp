7397cfa9667f312ab31b48ad48ece262d7a81ab8
Aditya Bang <adityabang15@gmail.com>
2025-03-26 23:44:54 -0400

    initial implementations of shouldAcceptTransfer and onTransferREception

--- stm32l552xx/boardfiles/drivers/can/can.cpp
+++ stm32l552xx/boardfiles/drivers/can/can.cpp
@@ -17,3 +17,37 @@ CAN::CAN() {
 CAN::~CAN() {
 
 }
+
+bool CAN::CanardShouldAcceptTransfer(
+	const CanardInstance *ins,
+	uint64_t *out_data_type_signature,
+	uint16_t data_type_id,
+	CanardTransferType transfer_type,
+	uint8_t source_node_id)
+{
+	if (transfer_type == CanardTransferTypeRequest) {
+		// check if we want to handle a specific service request
+		switch (data_type_id) {
+			case UAVCAN_PROTOCOL_GETNODEINFO_ID: {
+				*out_data_type_signature = UAVCAN_PROTOCOL_GETNODEINFO_REQUEST_SIGNATURE;
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+void CAN::onTransferReceived(
+	CanardInstance *ins,
+	CanardRxTransfer *transfer)
+{
+    if (transfer->transfer_type == CanardTransferTypeRequest) {
+        // check if we want to handle a specific service request
+        switch (transfer->data_type_id) {
+            case UAVCAN_PROTOCOL_GETNODEINFO_ID: {
+                handle_GetNodeInfo(ins, transfer); // TODO need to implement this function
+                break;
+            }
+        }
+    }
+}
