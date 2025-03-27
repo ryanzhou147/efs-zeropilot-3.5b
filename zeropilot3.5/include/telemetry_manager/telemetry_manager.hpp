0d72de7eb051a58367cf20d6f027da36bbd54e03
dechair3 <dechair3@gmail.com>
2025-03-27 00:01:32 -0400

    Replace circular buffer with queue drivers

--- zeropilot3.5/include/telemetry_manager/telemetry_manager.hpp
+++ zeropilot3.5/include/telemetry_manager/telemetry_manager.hpp
@@ -1,19 +1,17 @@
 #pragma once
-
+#define MAVLINK_MSG_MAX_SIZE 280
 #include <mavlink2/common/mavlink.h>
-#include "tm_circular_buffer.hpp"
 #include "queue_iface.hpp"
 #include "tm_queue.hpp"
-#include "rfd900_iface.hpp"
+#include "rfd_iface.hpp"
 class TelemetryManager {
   private:
-    TMCircularBuffer highPriorityTxBuffer_ = TMCircularBuffer(); // GPOS, Attitude and Heartbeat/Connection Messages
-    TMCircularBuffer lowPriorityTxBuffer_ = TMCircularBuffer();  // Everything Else (to be defined)
-    IMessageQueue<TMMessage_t> *tmQueueDriver_;                   // Driver that receives messages from other managers
-    IRFD900 *rfdDriver_;                                         // Driver used to actually send mavlink messages
+    IMessageQueue<mavlink_message_t> *messageBuffer_{};            // GPOS, Attitude and Heartbeat/Connection Messages
+    IMessageQueue<TMMessage_t> *tmQueueDriver_;                  // Driver that receives messages from other managers
+    IRFD *rfdDriver_;                                         // Driver used to actually send mavlink messages
 
   public:
-    TelemetryManager(IRFD900 *rfdDriver, IMessageQueue<TMMessage_t>  *tmQueueDriver);
+    TelemetryManager(IRFD *rfdDriver, IMessageQueue<TMMessage_t>  *tmQueueDriver, IMessageQueue<mavlink_message_t> *messageBuffer);
     ~TelemetryManager();
 
     void processMsgQueue();
--- zeropilot3.5/src/telemetry_manager/telemetry_manager.cpp
+++ zeropilot3.5/src/telemetry_manager/telemetry_manager.cpp
@@ -3,8 +3,8 @@
 #define COMPONENT_ID 1          // Suggested Component ID by MAVLINK
 
 
-TelemetryManager::TelemetryManager(IRFD900 *rfdDriver, IMessageQueue<TMMessage_t> *tmQueueDriver):
-    tmQueueDriver_(tmQueueDriver), rfdDriver_(rfdDriver) {
+TelemetryManager::TelemetryManager(IRFD *rfdDriver, IMessageQueue<TMMessage_t> *tmQueueDriver, IMessageQueue<mavlink_message_t> *messageBuffer):
+    tmQueueDriver_(tmQueueDriver), rfdDriver_(rfdDriver), messageBuffer_(messageBuffer) {
 
 }
 
@@ -12,7 +12,7 @@ TelemetryManager::~TelemetryManager() = default;
 
 
 void TelemetryManager::processMsgQueue() {
-    while (true) {
+    while (tmQueueDriver_->count() > 0) {
         mavlink_message_t mavlink_message = {};
         TMMessage_t tmq_message = tmQueueDriver_->pop();
 
@@ -29,13 +29,13 @@ void TelemetryManager::processMsgQueue() {
                 break;
             case TMMessage_t::BM_DATA: //Basically a placeholder, Check in with BM team later
                 auto BMData = tmq_message.tm_message_data.BMData_t;
-                mavlink_msg_battery_status_pack(SYSTEM_ID, COMPONENT_ID, &mavlink_message, 255, MAV_BATTERY_FUNCTION_UNKNOWN, MAV_BATTERY_TYPE_UNKNOWN,
+                mavlink_msg_battery_status_pack(SYSTEM_ID, COMPONENT_ID, &mavlink_message, 255, MAV_BATTERY_FUNCTION_UNKNOWN, MAV_BATTERY_TYPE_LIPO,
                  BMData.temperature, BMData.voltages, BMData.current_battery, BMData.current_consumed, BMData.energy_consumed, BMData.battery_remaining,
                  BMData.time_remaining, BMData.charge_state, BMData.voltages_ext, 0, 0);
             case default:
                 //WHOOPS
         }
-        highPriorityTxBuffer_.enqueue(mavlink_message);
+        messageBuffer_->push(mavlink_message);
     }
 }
 
@@ -47,18 +47,16 @@ void TelemetryManager::heartBeatMsg() {
 
     mavlink_msg_heartbeat_pack(SYSTEM_ID, COMPONENT_ID, &heartbeat_message, MAV_TYPE_QUADROTOR, MAV_AUTOPILOT_INVALID,
                                base_mode, 0, system_status);
-    highPriorityTxBuffer_.enqueue(heartbeat_message);
+    messageBuffer_->push(heartbeat_message);
 }
 
 void TelemetryManager::transmit() {
+    uint8_t transmit_buffer[MAVLINK_MSG_MAX_SIZE];
     mavlink_message_t msg_to_tx{};
-        while (highPriorityTxBuffer_.dequeue(msg_to_tx)) {
-            mavlink_msg_to_send_buffer(rfdDriver_->buf_, &msg_to_tx);
-            rfdDriver_->transmit();
-        }
-        while (lowPriorityTxBuffer_.dequeue(msg_to_tx)) {
-            mavlink_msg_to_send_buffer(rfdDriver_->buf_, &msg_to_tx);
-            rfdDriver_->transmit();
+        while (messageBuffer_->count() > 0) {
+            msg_to_tx = messageBuffer_->pop();
+            const uint8_t msg_len = mavlink_msg_to_send_buffer(transmit_buffer, &msg_to_tx);
+            rfdDriver_->transmit(transmit_buffer, msg_len);
         }
 }
 
