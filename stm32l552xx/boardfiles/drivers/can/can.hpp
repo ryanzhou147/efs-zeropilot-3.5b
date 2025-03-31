3aa3207368bf47eb35e0b53322d18a38f12bcb1b
Ronak Patel <98669069+Rosnaky@users.noreply.github.com>
2025-03-31 13:38:15 -0400

    removed unistd

--- stm32l552xx/boardfiles/drivers/can/can.hpp
+++ stm32l552xx/boardfiles/drivers/can/can.hpp
@@ -1,7 +1,6 @@
 
 #pragma once
 
-#include <unistd.h>
 #include <can_iface.hpp>
 #include "can_datatypes.hpp"
 #include <canard.h>
@@ -11,18 +10,14 @@
 #include <map>
 
 // need to handle
-#define CAN_BROADCAST_MUTEX_TIMEOUT UINT32_MAX
+#define CAN_BROADCAST_MUTEX_TIMEOUT HAL_MAX_DELAY
 
 class CAN : ICAN {
 
 private:
 	std::map<uint8_t, Node_t> canNodes;
-	uint8_t nextAvailableID = 1;
-
-
+	uint8_t nextAvailableID = 2;
 	FDCAN_HandleTypeDef *hfdcan;
-
-	// Returns the id of the allocated node
 	
 	CanardInstance canInst;
 
@@ -40,46 +35,23 @@ private:
 public:
 	CAN(FDCAN_HandleTypeDef* hfdcan);
 	CAN();
-
 	virtual ~CAN();
 
-	int16_t canardSTM32Receive(FDCAN_HandleTypeDef *hfdcan, uint32_t RxLocation, CanardCANFrame *const rx_frame);
-	
 	bool routineTasks();
 
+	int16_t canardSTM32Receive(FDCAN_HandleTypeDef *hfdcan, uint32_t RxLocation, CanardCANFrame *const rx_frame);
 
-	/**
-	 * Wrapper function with mutex
-	 * 
-	* Sends a broadcast transfer.
-	* If the node is in passive mode, only single frame transfers will be allowed (they will be transmitted as anonymous).
-	*
-	* For anonymous transfers, maximum data type ID (CanardTxTransfer::data_type_id) is limited to 3 (see specification for details).
-	*
-	* Please refer to the specification for more details about data type signatures (CanardTxTransfer::data_type_signature). Signature for 
-	* any data type can be obtained in many ways; for example, using the generated code generated using dronecan_dsdlc (see the repository).
-	*
-	* Use CanardTxTransfer structure to pass the transfer parameters. The structure is initialized by the
-	* canardInitTxTransfer() function.
-	* 
-	* Pointer to the Transfer ID (CanardTxTransfer::inout_transfer_id) should point to a persistent variable
-	* (e.g. static or heap allocated, not on the stack); it will be updated by the library after every transmission. 
-	* The Transfer ID value cannot be shared between transfers that have different descriptors!
-	* More on this in the transport layer specification.
-	*
-	* Returns the number of frames enqueued, or negative error code.
-	*/
 	int16_t broadcastObj(
-		CanardTxTransfer* transfer      ///< Transfer object
+		CanardTxTransfer* transfer
 	);
 
-	int16_t broadcast(            ///< Library instance
-		uint64_t data_type_signature,   ///< See above
-		uint16_t data_type_id,          ///< Refer to the specification
-		uint8_t* inout_transfer_id,     ///< Pointer to a persistent variable containing the transfer ID
-		uint8_t priority,               ///< Refer to definitions CANARD_TRANSFER_PRIORITY_*
-		const void* payload,            ///< Transfer payload
+	int16_t broadcast(
+		uint64_t data_type_signature,
+		uint16_t data_type_id,
+		uint8_t* inout_transfer_id,
+		uint8_t priority,
+		const void* payload,
 		uint16_t payload_len
-	)
+	);
 
 };
