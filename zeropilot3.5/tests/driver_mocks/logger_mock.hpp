f4af0b23d55859ba65283fb8fbf307c173867c91
eGuitar999 <ari.vancruyningen@gmail.com>
2025-04-05 14:43:07 -0400

    gtest build and example test

--- zeropilot3.5/tests/driver_mocks/logger_mock.hpp
+++ zeropilot3.5/tests/driver_mocks/logger_mock.hpp
@@ -6,5 +6,4 @@
 class MockLogger : public ILogger {
     MOCK_METHOD(int, log, (char message[100]), (override));
     MOCK_METHOD(int, log, (char messages[][100], int count), (override));
-    MOCK_METHOD(int, init, (), (override));
 };
