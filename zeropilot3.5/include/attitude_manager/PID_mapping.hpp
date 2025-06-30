b908ea986ebbba98b32eb185644c5edcdde22ca4
Eeman Aleem <eemanaleem@gmail.com>
2025-06-30 01:44:21 -0400

    Added pitch controls to runControls func in PID_mapping hpp & cpp

--- zeropilot3.5/include/attitude_manager/PID_mapping.hpp
+++ zeropilot3.5/include/attitude_manager/PID_mapping.hpp
@@ -7,4 +7,14 @@ class PIDMapping : public Flightmode {
         PIDMapping() = default;
 
         RCMotorControlMessage_t runControl(RCMotorControlMessage_t controlInput) override;
+    private:
+        // Pitch controls' PID constants
+        float pitch_Kp = 0.0f;
+        static float pitch_Ki = 0.0f;
+        float pitch_Kd = 0.0f;
+
+        // PID state variables
+        float previousPitchError = 0.0f;
+        float pitchIntegral = 0.0f;
+        float pitchSampleTime = 0.001f;  // Sample time in seconds (100ms)
 };
--- zeropilot3.5/src/attitude_manager/pid_mapping.cpp
+++ zeropilot3.5/src/attitude_manager/pid_mapping.cpp
@@ -1,5 +1,26 @@
 #include "PID_mapping.hpp"
 
 RCMotorControlMessage_t PIDMapping::runControl(RCMotorControlMessage_t controlInputs){
+    // Current State
+    float pitchMeasured = controlInputs.pitch; // What is the band of values this can take? Ask limit of pitch
+    float pitchSetpoint = 50.0f;
+
+    // Error Calculation
+    float pitchError = pitchSetpoint - pitchMeasured;
+    float pitchDerivative = (pitchError - previousPitchError) / pitchSampleTime;
+    pitchIntegral += pitchError * pitchSampleTime;
+
+    // PID Computation
+    float pitchControlEffort = (pitch_Kp * pitchError) + (pitch_Kd * pitchDerivative) + (pitch_Ki * pitchIntegral);
+    previousPitchError = pitchError;
+
+    float pitchOutput = pitchMeasured + pitchControlEffort;
+
+    // Clamp output. 100 is a placeholder.
+    if (pitchOutput > 100.0f) pitchOutput = 100.0f;
+    if (pitchOutput < 0.0f) pitchOutput = 0.0f;
+
+    // Set controlInput
+    controlInputs.pitch = pitchOutput;
     return controlInputs;
 }
