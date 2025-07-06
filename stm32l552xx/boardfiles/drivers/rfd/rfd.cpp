3f116f8db229e5f8b556f37688d86808d7d9d6d7
Aditya-Bang <65207430+Aditya-Bang@users.noreply.github.com>
2025-07-06 17:25:28 -0400

    Add RFD900 driver (#30)
    * making new branch feature/drivers/rfd
    
    * initial architecture files
    
    * rfd basic file structure
    
    * basic recieve transmit methods for rfd
    
    * receive constantly
    
    * a lot of issues but dma kinda working????
    
    * global rfd class
    
    * ensure correct huart
    
    * implemented circular buffer v1
    
    * small fix
    
    * rxBuffer to uint_8
    
    * fixed logic after tests
    
    * Added callback method & made improvements
    
    * clean up code
    
    * removed redundant HAL_UARTEx_recieveToIdle_DMA call
    
    * added request updates + include a reset function for error flag
    
    * Fixed race condition in receive function
    
    * simplified code
    
    ---------
    
    Co-authored-by: Shakthi-Viviyn <55011804+Shakthi-Viviyn@users.noreply.github.com>

---
+++ stm32l552xx/boardfiles/drivers/rfd/rfd.cpp
@@ -0,0 +1,52 @@
+#include "rfd.hpp"
+#include "stm32l5xx_hal_uart.h"
+
+/**
+ * Important consideraton when using this driver:
+ * Recieve should be called fast enough to never let the buffer overflow,
+ * since there is currently no way to recover from an overflow
+ */
+
+RFD* RFD::instance = nullptr; // global instance
+
+RFD::RFD(UART_HandleTypeDef* huart) : huart(huart), readIndex(0), writeIndex(0){
+    instance = this;
+}
+
+RFD::~RFD() {
+    instance = nullptr;
+}
+
+void RFD::transmit(uint8_t* data, uint16_t size) {
+    if (huart) {
+        HAL_UART_Transmit_DMA(huart, data, size);
+    }
+}
+
+void RFD::startReceive() {
+    if (huart) {
+        HAL_UARTEx_ReceiveToIdle_DMA(huart, rxBuffer, BUFFER_SIZE);
+    }
+}
+
+uint16_t RFD::receive(uint8_t* buffer, uint16_t bufferSize) {
+    for (uint16_t i = 0; i < bufferSize; i++) {
+        if (readIndex == writeIndex){
+            return i;
+        }
+        buffer[i] = rxBuffer[readIndex];
+        readIndex++;
+        if (readIndex >= BUFFER_SIZE) {
+            readIndex = 0;
+        }
+    }
+    return bufferSize;
+}
+
+void RFD::receiveCallback(uint16_t size){
+    writeIndex = size;
+}
+
+UART_HandleTypeDef* RFD::getHuart() const {
+    return huart;
+}
