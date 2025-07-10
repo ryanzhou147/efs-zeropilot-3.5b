c16d7241f16e678e2500ed92eccc11b7e187e11a
Eeman Aleem <eemanaleem@gmail.com>
2025-07-10 19:06:45 -0400

    Update attitude_manager.cpp

--- zeropilot3.5/src/attitude_manager/attitude_manager.cpp
+++ zeropilot3.5/src/attitude_manager/attitude_manager.cpp
@@ -77,12 +77,12 @@ void AttitudeManager::runControlLoopIteration() {
         motorOutputs.yaw = 0;
     }
     
-    outputToMotor(YAW, (100-motorOutputs.yaw)*if_yawMotors_invert+motorOutputs.yaw*(if_yawMotors_invert-1)+YAWMOTORS_TRIM);
-    outputToMotor(PITCH, (100-motorOutputs.pitch)*if_pitchMotors_invert+motorOutputs.pitch*(if_pitchMotors_invert-1)+PITCHMOTORS_TRIM);
-    outputToMotor(ROLL, (100-motorOutputs.roll)*if_rollMotors_invert+motorOutputs.roll*(if_rollMotors_invert-1)+ROLLMOTORS_TRIM);
-    outputToMotor(THROTTLE, (100-motorOutputs.throttle)*if_throttleMotors_invert+motorOutputs.throttle*(if_throttleMotors_invert-1)+THROTTLEMOTORS_TRIM);
-    outputToMotor(FLAP_ANGLE, (100-motorOutputs.flapAngle)*if_flapMotors_invert+motorOutputs.flapAngle*(if_flapMotors_invert-1)+FLAPMOTORS_TRIM);
-    outputToMotor(STEERING, motorOutputs.yaw+STEERINGMOTORS_TRIM);
+    outputToMotor(YAW, (100-motorOutputs.yaw)*ifYawMotorsInvert+motorOutputs.yaw*(ifYawMotorsInvert-1)+yawmotorsTrim);
+    outputToMotor(PITCH, (100-motorOutputs.pitch)*ifPitchMotorsInvert+motorOutputs.pitch*(ifPitchMotorsInvert-1)+pitchmotorsTrim);
+    outputToMotor(ROLL, (100-motorOutputs.roll)*ifRollMotorsInvert+motorOutputs.roll*(ifRollMotorsInvert-1)+rollmotorsTrim);
+    outputToMotor(THROTTLE, (100-motorOutputs.throttle)*ifThrottleMotorsInvert+motorOutputs.throttle*(ifThrottleMotorsInvert-1)+throttlemotorsTrim);
+    outputToMotor(FLAP_ANGLE, (100-motorOutputs.flapAngle)*ifFlapMotorsInvert+motorOutputs.flapAngle*(ifFlapMotorsInvert-1)+flapmotorsTrim);
+    outputToMotor(STEERING, motorOutputs.yaw+steeringmotorsTrim);
 }
 
 bool AttitudeManager::getControlInputs(RCMotorControlMessage_t *pControlMsg) {
