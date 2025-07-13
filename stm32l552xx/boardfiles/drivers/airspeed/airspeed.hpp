f7dd0f46698df9f54ce9aa8cbbbca7914915fe13
kariizum <kariizum@uwaterloo.ca>
2025-07-13 17:21:38 -0400

    initial code

---
+++ stm32l552xx/boardfiles/drivers/airspeed/airspeed.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "airspeed_iface.hpp"
+#include "stm3215xx_hal.h"
+
+class airspeed : public airspeed_iface
+{
+private:
+    /* data */
+public:
+    airspeed : public airspeed_iface(/* args */);
+    ~airspeed : public airspeed_iface();
+};
+
+airspeed : public airspeed_iface::airspeed : public airspeed_iface(/* args */)
+{
+}
+
+airspeed : public airspeed_iface::~airspeed : public airspeed_iface()
+{
+}
