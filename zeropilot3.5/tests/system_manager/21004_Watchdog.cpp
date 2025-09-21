c8b1c3f3b76e9d9979755e9d18e4e27a51a64c1d
Ryan <ryanawesome888@gmail.com>
2025-09-21 17:20:34 -0400

    Fixed watchdog unit test

--- zeropilot3.5/tests/system_manager/21004_Watchdog.cpp
+++ zeropilot3.5/tests/system_manager/21004_Watchdog.cpp
@@ -1,63 +1,36 @@
-#include <gmock/gmock.h>
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include "system_manager.hpp"
 #include "iwdg_mock.hpp"
+#include "logger_mock.hpp"
+#include "rc_mock.hpp"
+#include "queue_mock.hpp"
 
-using ::testing::Return;
-using ::testing::StrictMock;
 using ::testing::_;
-using ::testing::InSequence;
 using ::testing::NiceMock;
+using ::testing::StrictMock;
 
-//Test 1: Successful watchdog refresh
-TEST(WatchdogTest, RefreshWatchdog_ReturnsTrue){
-    StrictMock<MockWatchdog> mockWatchdog;
-    EXPECT_CALL(mockWatchdog, refreshWatchdog)
-        .WillOnce(Return(true));
-    
-    // Act and Assert in one step
-    EXPECT_TRUE(mockWatchdog.refreshWatchdog());
-};
+TEST(SMTest, 21004_Watchdog) 
+{  
+    StrictMock<MockWatchdog> mockIWDG;
+    NiceMock<MockLogger> mockLogger;
+    NiceMock<MockRCReceiver> mockRC;
+    NiceMock<MockMessageQueue<RCMotorControlMessage_t>> mockAmQueue;
+    NiceMock<MockMessageQueue<char[100]>> mockSmLoggerQueue;
 
-//Test 2: Watchdog refresh failure
+    SystemManager sm(&mockIWDG, &mockLogger, &mockRC, &mockAmQueue, &mockSmLoggerQueue);
 
-TEST(WatchdogTest, RefreshWatchdog_ReturnsFalse){
-    StrictMock<MockWatchdog> mockWatchdog;
-    EXPECT_CALL(mockWatchdog, refreshWatchdog())
-        .WillOnce(Return(false));
-    EXPECT_FALSE(mockWatchdog.refreshWatchdog());
-}
+    // Test 1: Verify watchdog is refreshed on every smUpdate() call
+    EXPECT_CALL(mockIWDG, refreshWatchdog());
 
-//Test 3: Multiple calls with different return values
+    sm.smUpdate();
 
-TEST(WatchdogTest, RefreshWatchdog_MultipleTimes){
-    StrictMock<MockWatchdog> mockWatchdog;
-    EXPECT_CALL(mockWatchdog, refreshWatchdog())
-        .WillOnce(Return(true))
-        .WillOnce(Return(true))
-        .WillOnce(Return(false))
-        .WillOnce(Return(true));
-    EXPECT_TRUE(mockWatchdog.refreshWatchdog());
-    EXPECT_TRUE(mockWatchdog.refreshWatchdog());
-    EXPECT_FALSE(mockWatchdog.refreshWatchdog());
-    EXPECT_TRUE(mockWatchdog.refreshWatchdog());
-}
-//Test 4: No expectations set, should return default value (false for bool)
-TEST(WatchdogTest, RefreshWatchdog_ReturnDefault){
-    NiceMock<MockWatchdog> mockWatchdog;
-    EXPECT_FALSE(mockWatchdog.refreshWatchdog());
+    // Test 2: Verify watchdog is called multiple times across multiple updates
+    EXPECT_CALL(mockIWDG, refreshWatchdog());
+    EXPECT_CALL(mockIWDG, refreshWatchdog());
+    EXPECT_CALL(mockIWDG, refreshWatchdog());
 
-}
-// // Test 5: Verify exact number of calls
-
-TEST(WatchdogTest, RefreshWatchdog_CalledExactNumberOfTimes) {
-    
-    StrictMock<MockWatchdog> mockWatchdog;
-    EXPECT_CALL(mockWatchdog, refreshWatchdog())
-        .Times(::testing::Exactly(20))
-        .WillRepeatedly(Return(false));
-
-    for (int i = 0; i < 20; ++i) {
-        mockWatchdog.refreshWatchdog();
-    }
-
+    sm.smUpdate();
+    sm.smUpdate();
+    sm.smUpdate();
 }
